d7d1f3100d371b3e30fa8474541bfd640c0651a2
Ricardo Nigrelli <ricanigrelli996@gmail.com>
2024-07-23 22:41:50 -0300

    no more hover

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,13 +4,10 @@ import React, { useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 
 function Services(props: { message: string; bg: string; href: string }) {
-  const [isHovered, setIsHovered] = useState(false);
-
   const content = (
     <div
       className="relative border border-gray-200 rounded-lg w-32 sm:w-72 h-fit bg-opacity-80 bg-[#FFFEFE] flex justify-center items-center hover:scale-110 transition-transform duration-300"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+
     >
       <div
         className={`-left-2 absolute w-3 h-3 rounded-full ${props.bg}`}
@@ -23,13 +20,9 @@ function Services(props: { message: string; bg: string; href: string }) {
 
   return (
     <div>
-      {isHovered ? (
-        <Link href={props.href} className="inline-block ">
-          {content}
-        </Link>
-      ) : (
-        content
-      )}
+      <Link href={props.href} className="inline-block ">
+        {content}
+      </Link>
     </div>
   );
 }
