99854b48f7afcaba7529454dc5bed4cd52576420
Carlos Ducrós <117871503+CarlosDucros@users.noreply.github.com>
2024-08-19 17:23:21 -0300

    Trabajando en el bg animado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { BubbleBg } from "@/components/BubbleBg";
 
 // Especificando los pesos de la fuente
 const poppins = Poppins({
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <BubbleBg>{children}</BubbleBg>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { BubbleBg } from "@/components/BubbleBg";
 import Globe from "@/components/Globe";
 import SelectLanguages from "@/components/SelectLanguages";
 import Image from "next/image";
